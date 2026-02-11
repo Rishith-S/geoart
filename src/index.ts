2a353c3cc8412d51df431385fa30b8e6fd7c973b
Rishith-S <rishithsaginala@gmail.com>
2026-02-11 13:42:18 -0800

    northflank

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import { renderPoster } from "./render";
 import { geocodeCity, reverseGeocode } from "./geocode";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(express.static('public'));
 app.use('/out', express.static('out'));
